eac836457a6c8771d04223c0ba17d25ff0372666
Ryan Sandoval <ryan_sandoval@live.com>
2025-02-17 21:34:50 -0800

    Improve metrics log format

--- hangoutsscheduler/utils/logging/metrics.py
+++ hangoutsscheduler/utils/logging/metrics.py
@@ -1,19 +1,31 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import time
+import os
 from typing import Optional
 from .logging_config import LOGGING_DIR
 
 MAX_LOG_SIZE_BYTES = 1024*1024*1024  # 1GB
 
+FORMAT_WITH_REQUEST_ID = "%(asctime)s\trequest_id:%(request_id)s,%(message)s"
+FORMAT_NO_REQUEST_ID = "%(asctime)s\t%(message)s"
+
 class MetricsLogger:
     """Logs metrics to a file.
-    
+
+    Metrics are outputted to the metrics.log file in the logs directory.
     Flush should be called at the end of each request.
+
+    Example of metric output:
+
+    ```plain
+    2025-02-17 21:30:07,517\trequest_id:b370c3ecc0,operation:LLMService.respond_to_message
+    ```
     """
     def __init__(self, request_id_filter=None, log_file='metrics.log', metrics_sublogger: Optional[str] = None):
         self.request_id_filter = request_id_filter
         self.log_file = log_file
+        self.metrics_buffer = {}
 
         if metrics_sublogger:
             self.logger = logging.getLogger('hangoutsscheduler.metrics.'+metrics_sublogger)
@@ -22,17 +34,20 @@ class MetricsLogger:
         self.logger.setLevel(logging.INFO)
 
         # Set up file handler
+        if not os.path.exists(LOGGING_DIR):
+            os.makedirs(LOGGING_DIR)
         handler = RotatingFileHandler(f'{LOGGING_DIR}/{log_file}', maxBytes=MAX_LOG_SIZE_BYTES, backupCount=5)
         handler.setLevel(logging.INFO)
-        handler.setFormatter(logging.Formatter('%(asctime)s\t%(request_id)s\t%(message)s'))
 
         if request_id_filter:
             handler.addFilter(request_id_filter)
+            handler.setFormatter(logging.Formatter(FORMAT_WITH_REQUEST_ID))
+        else:
+            handler.setFormatter(logging.Formatter(FORMAT_NO_REQUEST_ID))
 
         # Configure logger
         self.logger.handlers = [handler]
         self.logger.propagate = False
-        self.metrics_buffer = {}
 
 
     def __enter__(self):
